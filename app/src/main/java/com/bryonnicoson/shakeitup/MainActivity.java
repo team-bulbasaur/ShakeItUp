b5c7e8d471ce7dd94d4e286c90fa8bed9b0caf8b
Bryon Nicoson <bryon.nicoson@gmail.com>
2016-07-27 12:17:46 -0500

    added Restaurant model, process response to model

--- app/src/main/java/com/bryonnicoson/shakeitup/MainActivity.java
+++ app/src/main/java/com/bryonnicoson/shakeitup/MainActivity.java
@@ -11,6 +11,7 @@ import com.yelp.clientlib.entities.SearchResponse;
 import com.yelp.clientlib.entities.options.CoordinateOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,11 +20,14 @@ import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
 
-    // yelp object & YelpTask parameters  map
+    // TODO: move yelpTask, searchResponse processing to separate class? service? - ask James for feedback
+
+    // yelp object & YelpTask parameters map & arrayList of our Restaurant objects
     public SearchResponse searchResponse;
     public Map paramsMap = new HashMap<String, String>();
+    public ArrayList<Restaurant> restaurants = new ArrayList<>();
 
-    private class YelpTask extends AsyncTask<Map, Void, SearchResponse> {
+    private class YelpTask extends AsyncTask<Map, Void, com.yelp.clientlib.entities.SearchResponse> {
 
         @Override
         protected SearchResponse doInBackground(Map... parameters) {
@@ -62,9 +66,49 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onPostExecute(SearchResponse searchResponse) {
 
-            // TODO: Process searchResponse object here - for now, a test...
             for (int i = 0; i < searchResponse.businesses().size(); i++) {
-                Log.i("TEST", searchResponse.businesses().get(i).name().toString());
+                // get what we need from the yelp response object
+                String id = searchResponse.businesses().get(i).id().toString();
+                String name = searchResponse.businesses().get(i).name().toString();
+                String image = searchResponse.businesses().get(i).imageUrl().toString();
+                String website = searchResponse.businesses().get(i).mobileUrl().toString();
+                Double latitude = searchResponse.businesses().get(i).location().coordinate().latitude();
+                Double longitude = searchResponse.businesses().get(i).location().coordinate().longitude();
+                String snippetText = searchResponse.businesses().get(i).snippetText().toString();
+                Integer reviewCount = searchResponse.businesses().get(i).reviewCount();
+                String snippetImg = searchResponse.businesses().get(i).snippetImageUrl();
+                String ratingImg = searchResponse.businesses().get(i).ratingImgUrlLarge().toString();
+                String phone = searchResponse.businesses().get(i).displayPhone().toString();
+
+                // unpack and format categories and displayAddress lists
+                StringBuilder sb = new StringBuilder();
+                Integer categoriesListSize = searchResponse.businesses().get(i).categories().size();
+                for (int j = 0; j < categoriesListSize; j++) {
+                    sb.append(searchResponse.businesses().get(i).categories().get(j).name());
+                    if (j+1 < categoriesListSize)
+                        sb.append(" \u00B7 "); // put a middot between category names
+                }
+                String categories = sb.toString();
+                sb.setLength(0);
+                Integer addressListSize = searchResponse.businesses().get(i).location().displayAddress().size();
+                for (int k = 0; k < addressListSize; k++) {
+                    sb.append(searchResponse.businesses().get(i).location().displayAddress().get(k).toString());
+                    if (k+1 < addressListSize)
+                        sb.append("\n");  // if there's another element in addressList, put it on a new line
+                }
+                String address = sb.toString();
+
+                // construct new Restaurant object
+                Restaurant restaurant = new Restaurant(id, name, image, categories, address, phone,
+                        website, ratingImg, reviewCount, snippetImg, snippetText, latitude, longitude);
+
+                // add to arraylist
+                restaurants.add(restaurant);
+
+            }
+            // TODO: return restaurants to ui adapter - for now, logging to validate...
+            for (int inc = 0; inc < restaurants.size(); inc++) {
+                Log.i("RESTAURANTS", restaurants.get(inc).getName());
             }
         }
     }
@@ -81,10 +125,9 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: get search parameters from ui / db
         paramsMap.put("term", "food");
-        paramsMap.put("limit", "10");
+        paramsMap.put("limit", "20");  // fyi: 20 is max
 
         YelpTask yelpTask = new YelpTask();
         yelpTask.execute(paramsMap);
-
     }
 }
